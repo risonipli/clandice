9b3b5046ddc06bc2d6c3f4cdf7da02d1685dbeb0
Denis Yeremeev <droselmayer@yahoo.com>
2010-12-12 03:02:40 +0300

    [security] добавлена форма регистрации пользователя

---
+++ security/forms.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.contrib.auth.models import User
+
+class RegistrationForm(forms.Form):
+	"""
+	Класс представляет собой форму регистрации пользователя
+	"""
+
+	# фамилия регистрируемого пользователя
+	last_name = forms.CharField(label="Фамилия:", max_length=30,
+			required=False)
+	# имя регистрируемого пользователя
+	first_name = forms.CharField(label="Имя:", max_length=30, required=False)
+	# отчество регистрируемого пользователя
+	middle_name = forms.CharField(label = "Отчество:",
+			max_length=30,required=False)
+	# имя регистрируемого пользователя для входа в систему.
+	# Это имя будет запрашиваться на форме аутентификации
+	login_name = forms.CharField(label="Имя входа:", max_length=30)
+	# "никнейм" пользователя. Это имя будет использоваться на сайте (будут
+	# видеть другие пользователи)
+	nickname = forms.CharField(label="Никнейм:", max_length=30, required=False)
+	# адрес электронной почты регистрируемого пользователя
+	email = forms.EmailField(label="Адрес электронной почты:")
+	# пароль регистрируемого пользователя
+	password = forms.CharField(label="Пароль:", min_length=6, max_length=25,
+			widget=forms.PasswordInput())
+	# поле для проверки введенного пароля. Под проверкой здесь понимается
+	# проверка на возможность случайной опечатки при вводе пароля
+	password_confirm = forms.CharField(label="Потверждение пароля:",
+			min_length=6, max_length=25, widget=forms.PasswordInput())
+
+	def clean_login_name(self):
+		"""
+		Проверяет на существование в системе пользователя с таким же именем
+		входа. Если введеное значение имени еще не занято, то возвращается
+		введенное значение, иначе - ошибка с сообщением, что такое имя уже
+		существует
+		"""
+		login_name = self.cleaned_data['login_name']
+		try:
+			User.objects.get(username=login_name)
+			raise forms.ValidationError("Данное имя уже используется.
+					Пожалуйста, замените на другое.")
+		except User.DoesNotExist:
+			# если пользователя не нашлось
+			pass
+
+		return login_name
+
+	def clean_password_confirm(self):
+		"""
+		Проверяет введенный пароль на возможность случайной опечатки при вводе
+		"""
+		pwd = self.cleaned_data['password_confirm']
+		if self.cleaned_data['password'] != pwd:
+			raise forms.ValidationError("Пароль и потверждение пароля не
+					совпадают!")
+
+		return pwd
+
--- security/tests.py
+++ security/tests.py
@@ -1,23 +1,56 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
+# -*- coding: utf-8 -*-
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from security.forms import RegistrationForm
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class RegistrationFormTest(TestCase):
 
->>> 1 + 1 == 2
-True
-"""}
+	def test_form_validation(self):
+		"""
+		Тестирует работоспособность формы регистрации пользователя
+		"""
+		# полная верная информация
+		valid_full_data = {'last_name': 'Петров', 'first_name': 'Петр', 'middle_name':
+				'Петрович', 'login_name': 'petr88', 'nickname': 'petroman',
+				'email': 'petro88@mail.com', 'password':
+				'qwerty', 'password_confirm': 'qwerty'}
+
+		# основная верная информация
+		valid_base_data = {'login_name': 'petr88', 'email': 'petro88@mail.com',
+				'password': 'qwerty', 'password_confirm': 'qwerty'}
+
+		# неверная информация - отсутствует обязательное значени имени входа
+		invalid_data_blank_login_name = {'email': 'petro88@mail.com',
+				'password': 'qwerty', 'password_confirm': 'qwerty'}
+
+		# неверная информация - отсутствует обязательное значение адреса
+		# электронной почты
+		invalid_data_blank_email = {'login_name': 'petr88', 'password':
+				'qwerty', 'password_confirm': 'qwerty'}
+
+		# неверная информация - неверное значение адреса электронной почты
+		invalid_data_invalid_email = {'login_name': 'petr88', 'email':
+				'petro88.mail.com', 'password': 'qwerty', 'password_confirm':
+				'qwerty'}
+
+		# неверная информация - значение потверждения пароля содержит опечатку
+		# (вместо латинской 'e' введено русская 'е')
+		invalid_data_various_pwds = {'login_name': 'petr88', 'email':
+				'petro88@mail.com', 'password': 'qwerty', 'password_confirm':
+				'qwеrty'}
+
+		reg_form1 = RegistrationForm(valid_full_data)
+		reg_form2 = RegistrationForm(valid_base_data)
+		reg_form3 = RegistrationForm(invalid_data_blank_login_name)
+		reg_form4 = RegistrationForm(invalid_data_blank_email)
+		reg_form5 = RegistrationForm(invalid_data_invalid_email)
+		reg_form6 = RegistrationForm(invalid_data_various_pwds)
+
+		self.assertTrue(reg_form1.is_valid(), "valid full data")
+		self.assertTrue(reg_form2.is_valid(), "valid base data")
+		self.assertFalse(reg_form3.is_valid(), "invalid data: blank login_name")
+		self.assertFalse(reg_form4.is_valid(), "invalid data: blank email")
+		self.assertFalse(reg_form5.is_valid(), "invalid_data: invalid email")
+		self.assertFalse(reg_form6.is_valid(), "invalid data: various pwds")
 
