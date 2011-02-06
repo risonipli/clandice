3f3c898dac52f42a5e39211ccc09b6cba9271c0d
Denis Yeremeev <droselmayer@yahoo.com>
2011-02-06 15:41:13 +0300

    [blog] добавлена модель для сущностей 'пост' и 'комментарий'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+from treebeard.ns_tree import NS_Node
+
+from common.models import AbstractMessage
+
+class Post(AbstractMessage, NS_Node):
+	"""
+	Данный класс представляет собой сущности "пост" и "комментарии".
+	Организует древовидную структуру данных
+	"""
+
+	# контент будет доступен всем пользователям или только
+	# зарегистрированным
+	is_public = models.BooleanField("Виден всем?", default=True)
+	# ip-адрес пользователя, который изменил сообщение
+	ip_change_by = models.IPAddressField("IP-адрес пользователя, \
+			изменившего пост", blank=True, null=True)
+	# node_order_by = ["created_on"]
+
+	class Meta:
+		ordering = ["tree_id", "lft"]
+
--- blog/tests.py
+++ blog/tests.py
@@ -1,23 +1,65 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+# -*- coding: utf-8 -*-
 
-Replace these with more appropriate tests for your application.
-"""
-
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from blog.models import Post
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class PostTest(TestCase):
 
->>> 1 + 1 == 2
-True
-"""}
+	def test_create_post(self):
+		"""
+		"""
+		# пользователь раз
+		user1 = User.objects.create_user("user1", "user1@mail.com",
+				"user1pwd")
+		# пользователь два
+		user2 = User.objects.create_user("user2", "user2@mail.com",
+				"user2pwd")
+		# пользователь три
+		user3 = User.objects.create_user("user3", "user3@mail.com",
+				"user3pwd")
+
+		# создаем корень "дерева"
+		# FIXME корень не должен создавать пользователь!
+		root = Post.add_root(title="root title", message="root message",
+				created_by=user1)
+		self.assertNotEquals(root, None)
+
+		# второй пользователь создал пост
+		node0 = root.add_child(title="node0 title",
+				message="node0d message", created_by=user2)
+		# третий пользователь прочитал пост
+		node0.who_read.add(user3)
+		self.assertNotEquals(node0, None)
+
+		# третий пользователь создал пост
+		node1 = node0.add_sibling(title="node1 title", message="node1
+		message", created_by=user3)
+		# первый пользователь прочитал пост
+		node1.who_read.add(user1)
+		self.assertNotEquals(node1, None)
+
+		# первый пользователь создал комментарий к посту №1
+		node2 = node1.add_child(title="node2 comment for node1 title",
+				message="node2 comment for node1 message",
+				created_by=user1)
+		# второй пользователь прочитал пост №1
+		node1.who_read.add(user2)
+		# второй пользователь прочитал комментарий первого пользователя
+		node2.who_read.add(user2)
+		self.assertNotEquals(node2, None)
+
+		# второй пользователь создал комментарий к посту №1
+		node3 = node2.add_sibling(title="node3 comment for node1 title",
+				message="node3 comment for node1 message",
+				created_by=user2)
+		self.assertNotEquals(node3, None)
+
+		# третий пользователь откомментировал комментарий первого
+		# пользователя
+		node4 = node2.add_child(title="node4 comment for node2 title",
+				message="node4 comment for node2 message",
+				created_by=user3)
+		self.assertNotEquals(node4, None)
 
