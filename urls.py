699894c39990593ff89f39f8bd44ecd022b95802
Denis Yeremeev <droselmayer@yahoo.com>
2011-07-16 20:04:04 +0400

    активация интерфейса администратора

--- urls.py
+++ urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^clandice/', include('clandice.foo.urls')),
+    # Examples:
+    # url(r'^$', 'clandise.views.home', name='home'),
+    # url(r'^clandise/', include('clandise.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
 )
